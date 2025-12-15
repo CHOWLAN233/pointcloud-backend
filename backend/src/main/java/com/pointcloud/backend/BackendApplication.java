8f1947cbe86d17dc3780e3a41818751e4cde0b86
Raining777 <3436496959@qq.com>
2025-12-15 15:41:37 +0800

    Refactor import statement and comments in BackendApplication

--- backend/src/main/java/com/pointcloud/backend/BackendApplication.java
+++ backend/src/main/java/com/pointcloud/backend/BackendApplication.java
@@ -1,11 +1,11 @@
 package com.pointcloud.backend;
 
-import org.mybatis.spring.annotation.MapperScan; // 导入包
+import org.mybatis.spring.annotation.MapperScan;//Import package
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.pointcloud.backend.mapper") // <--- 加上这一行，指向你的 mapper 包
+@MapperScan("com.pointcloud.backend.mapper")//Point to the mapper package
 public class BackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
